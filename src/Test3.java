500aea5a3088dbe2b77d3778c2460c397e7772f0
SerzhSeryozhka <Jk3103KS@yandex.ru>
2024-04-11 13:07:21 +0300

    Initial commit

---
+++ src/Test3.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Test3 {
+    public static void N3() {
+        System.out.println("Как тебя зовут?");
+        Scanner scanner = new Scanner(System.in);
+        String q = scanner.next();
+        int e=q.length();
+        String a="а";
+        String i="и";
+        String y="я";
+        System.out.println(q.charAt(e));
+//        if ()
+            System.out.println("Приветик!");
+//        else
+            System.out.println("Здарово!");
+    }
+}
